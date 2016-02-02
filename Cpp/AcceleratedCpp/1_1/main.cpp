8af061d69210883e91f73c94a81c26f325346273
jerusalemdax <jerusalemdax@gmail.com>
2016-02-02 20:23:30 +0800

    增加第一章例子

---
+++ Cpp/AcceleratedCpp/1_1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "Please enter you name:" << endl;
+	string name;
+	cin >> name;
+	cout << "Hello, " << name << "!" << endl;
+	return 0;
+}

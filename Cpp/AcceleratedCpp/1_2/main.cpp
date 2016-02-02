9c8f628ebddd65a34f231a3bce71c3f233ddf193
jerusalemdax <jerusalemdax@gmail.com>
2016-02-02 21:06:52 +0800

    增加第一章例子2

---
+++ Cpp/AcceleratedCpp/1_2/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "Please enter you name:" << endl;
+	string name;
+	cin >> name;
+
+	const string greeting = "Hello, " + name + "!";
+	const string spaces(greeting.size(), ' ');
+	const string second = "* " + spaces + " *";
+	const string first(second.size(), '*');
+	cout << endl;
+	cout << first << endl;
+	cout << second << endl;
+	cout << "* " << greeting << " *" << endl;
+	cout << second << endl;
+	cout << first << endl;
+
+	return 0;
+}
